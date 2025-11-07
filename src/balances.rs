d7786c1eedf5b7699f129b7447537ef49e91952c
hrugved <whrugved@gmail.com>
2025-11-07 12:45:00 +0530

    add generic types for balance types

--- src/balances.rs
+++ src/balances.rs
@@ -1,35 +1,41 @@
 use std::collections::BTreeMap;
 
+use num::{CheckedAdd, CheckedSub, Zero};
+
 #[derive(Debug)]
-pub struct Pallet{
-    balances: BTreeMap<String, u128>
+pub struct Pallet<AccountId, Balance>{
+    balances: BTreeMap<AccountId, Balance>
 }
 
-impl Pallet {
+impl<AccountId, Balance> Pallet<AccountId, Balance> 
+where 
+    AccountId: Ord + Clone,
+    Balance: Zero + CheckedSub + CheckedAdd + Copy,
+{
     pub fn new() -> Self {
         Self {
             balances: BTreeMap::new()
         }
     }
 
-    pub fn set_balance(&mut self, who: &String, amount: u128) {
+    pub fn set_balance(&mut self, who: &AccountId, amount: Balance) {
         self.balances.insert(who.clone(), amount);
     }
 
-    pub fn balance(&self, who: &String) -> u128 {
-        *self.balances.get(who).unwrap_or(&0)
+    pub fn balance(&self, who: &AccountId) -> Balance {
+        *self.balances.get(who).unwrap_or(&Balance::zero())
     }
 
-    pub fn transfer(&mut self, caller: &String, to: &String, amount: u128) -> Result<(), &'static str> {
+    pub fn transfer(&mut self, caller: &AccountId, to: &AccountId, amount: Balance) -> Result<(), &'static str> {
         let caller_balance = self.balance(&caller);
         let to_balance = self.balance(&to);
         
         let new_caller_balance = caller_balance
-            .checked_sub(amount)
+            .checked_sub(&amount)
             .ok_or("Insufficient balance")?;
 
         let new_to_balance = to_balance
-            .checked_add(amount)
+            .checked_add(&amount)
             .ok_or("Overflow when adding to balance")?;
 
         self.set_balance(&caller, new_caller_balance);
--- src/system.rs
+++ src/system.rs
@@ -1,42 +1,50 @@
 use std::collections::BTreeMap;
 
+type AccountId = String;
+type BlockNumber = u32;
+type Nonce = u32;
+
 #[derive(Debug)]
 pub struct Pallet {
-    block_number: u32,
-    nonce: BTreeMap<String,u32>, // wallet to #transactions
+    block_number: BlockNumber,
+    nonce: BTreeMap<String, Nonce>, // wallet to #transactions
 }
 
 impl Pallet {
     pub fn new() -> Self {
         Self {
             block_number: 0,
-            nonce: BTreeMap::new()
+            nonce: BTreeMap::new(),
         }
     }
 
     pub fn inc_block_number(&mut self) {
-        self.block_number = self.block_number.checked_add(1).expect("Blockchain just crashed due to overflow")
+        self.block_number = self
+            .block_number
+            .checked_add(1)
+            .expect("Blockchain just crashed due to overflow")
     }
 
-    pub fn block_number(&self) -> u32 {
+    pub fn block_number(&self) -> BlockNumber {
         self.block_number
     }
 
-    pub fn inc_nonce(&mut self, who: &String) {
+    pub fn inc_nonce(&mut self, who: &AccountId) {
         let curr_nonce = self.nonce.get(who).unwrap_or(&0);
-        let new_nonce = curr_nonce.checked_add(1).unwrap_or_else(|| panic!("Nonce overflow for {}", who));
+        let new_nonce = curr_nonce
+            .checked_add(1)
+            .unwrap_or_else(|| panic!("Nonce overflow for {}", who));
         self.nonce.insert(who.clone(), new_nonce);
     }
 
-    pub fn get_nonce(&self, who: &String) -> u32 {
+    pub fn nonce(&self, who: &AccountId) -> Nonce {
         *self.nonce.get(who).unwrap_or(&0)
     }
-
 }
 
 #[cfg(test)]
 mod tests {
-    
+
     #[test]
     fn init_sytem() {
         let system = super::Pallet::new();
@@ -52,10 +60,9 @@ mod tests {
 
     #[test]
     fn inc_nonce() {
-        let mut system = super::Pallet::new(); 
+        let mut system = super::Pallet::new();
         let alice = &"alice".to_string();
         system.inc_nonce(alice);
-        assert_eq!(system.get_nonce(alice), 1);
+        assert_eq!(system.nonce(alice), 1);
     }
-
 }
