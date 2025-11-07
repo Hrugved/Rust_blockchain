0ab700aee4460f4549fab9ea2d77c0def1f05f3e
hrugved <whrugved@gmail.com>
2025-11-07 11:09:52 +0530

    add transfer functionality

--- src/balances.rs
+++ src/balances.rs
@@ -19,5 +19,79 @@ impl Pallet {
         *self.balances.get(who).unwrap_or(&0)
     }
 
+    pub fn transfer(&mut self, caller: &String, to: &String, amount: u128) -> Result<(), &'static str> {
+        let caller_balance = self.balance(&caller);
+        let to_balance = self.balance(&to);
+        
+        let new_caller_balance = caller_balance
+            .checked_sub(amount)
+            .ok_or("Insufficient balance")?;
+
+        let new_to_balance = to_balance
+            .checked_add(amount)
+            .ok_or("Overflow when adding to balance")?;
+
+        self.set_balance(&caller, new_caller_balance);
+        self.set_balance(&to, new_to_balance);
+
+        Ok(())
+    }
+
 }
 
+
+#[cfg(test)]
+mod tests {
+    use std::u128;
+
+
+    #[test]
+    fn init_balance() {
+        let mut balances = super::Pallet::new();
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+        balances.set_balance(&"alice".to_string(), 100);
+        assert_eq!(balances.balance(&"alice".to_string()), 100);
+        assert_eq!(balances.balance(&"bob".to_string()), 0);
+    }
+
+    #[test]
+    fn transfer_balance() {
+        let alice = "alice".to_string();
+        let bob = "bob".to_string();
+        let mut balances = super::Pallet::new();
+        balances.set_balance(&alice, 100);
+        let _ = balances.transfer(&alice, &bob, 50);
+        assert_eq!(balances.balance(&alice), 50);
+        assert_eq!(balances.balance(&bob), 50);
+    }
+
+    #[test]
+    fn transfer_balance_insufficient() {
+        let alice = "alice".to_string();
+        let bob = "bob".to_string();
+        let mut balances = super::Pallet::new();
+        balances.set_balance(&alice, 100);
+        let result = balances.transfer(&alice, &bob, 110);
+        assert_eq!(result, Err("Insufficient balance"));
+        assert_eq!(balances.balance(&alice), 100);
+        assert_eq!(balances.balance(&bob), 0);
+    }
+
+    
+
+    #[test]
+    fn transfer_balance_overflow() {
+        let alice = "alice".to_string();
+        let bob = "bob".to_string();
+        let mut balances = super::Pallet::new();
+        balances.set_balance(&alice, 100);
+        balances.set_balance(&bob, u128::MAX);
+        let result = balances.transfer(&alice, &bob, 10);
+        assert_eq!(result, Err("Overflow when adding to balance"));
+        assert_eq!(balances.balance(&alice), 100);
+        assert_eq!(balances.balance(&bob), u128::MAX);
+    }
+
+}
+
+ 
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,3 @@ fn main() {
     let pallet = balances::Pallet::new();
 }
  
-
-#[test]
-fn init_balance() {
-    let mut balances = balances::Pallet::new();
-    assert_eq!(balances.balance(&"alice".to_string()), 0);
-    balances.set_balance(&"alice".to_string(), 100);
-    assert_eq!(balances.balance(&"alice".to_string()), 100);
-    assert_eq!(balances.balance(&"bob".to_string()), 0);
-}
